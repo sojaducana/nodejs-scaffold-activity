c558e069d36e16c988c24dfaed1f422e2b2d20c3
Shane Oliver Jaducana <shane@fbmsoftware.com>
2017-01-21 16:02:52 +0800

    My changes

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -13,9 +13,31 @@ export let UserController = {
     this.status = 201;
   },
   findOne: function* () {
+    console.log(this.params.id);
+
+    let user = yield UserModel.findById(this.params.id);
+    if (user) {
+      console.log(user._id);
+      this.body = {
+        data: user
+      };
+      this.status = 200;
+    } else {
+      this.status = 404;
+    }
   },
   find: function* () {
+    let users = yield UserModel.find();
+    this.body = {
+      data: users
+    };
+    this.status = 200;
   },
   remove: function* () {
+    console.log(this.params.id);
+
+    yield UserModel.removeById(this.params.id);
+
+    this.status = 204;
   }
 };
--- src/test/user.js
+++ src/test/user.js
@@ -61,4 +61,104 @@ describe('User', function() {
       });
     });
   });
+
+  describe('GET /user/:id', function() {
+    let params = {
+          username: 'zenoan',
+          email: 'roger.madjos@gmail.com'
+        };
+
+    let userId;
+    before(function* () {
+      let createResult = yield request
+        .post('/users')
+        .send(params);
+      userId = createResult.body.data._id;
+    });
+
+    describe('Given user is found', function() {
+      it('Should return status 200', function* () {
+        yield request
+          .get('/users/'+userId)
+          .expect(200)
+          .expect(res => {
+            expect(res.body.data).to.has.property('_id', userId);
+          });
+      });
+    });
+
+    describe('Given user is not found', function() {
+      it('Should return status 404', function* () {
+        yield request
+          .get('/users/unknown')
+          .expect(404);
+      });
+    });
+  });
+
+  describe('GET /users', function() {
+    describe('Given valid request', function() {
+      it('Should return status 200', function* () {
+        yield request
+          .get('/users')
+          .expect(200);
+      });
+    });
+
+    // let params = {
+    //       username: 'zenoan',
+    //       email: 'roger.madjos@gmail.com'
+    //     };
+
+    // let userId;
+    // let users = {};
+    // before(function* () {
+    //   let createResult = yield request
+    //     .post('/users')
+    //     .send(params);
+    //   userId = createResult.body.data._id;
+
+    //   let user = yield request
+    //       .get('/users/'+userId);
+
+    //   users.push(user);
+    // });
+
+    // describe('Given there is one user created', function() {
+    //   it('Should return size of 1', function* () {
+    //     yield request
+    //       .get('/users')
+    //       .expect(200)
+    //       .expect(res => {
+    //         expect(users.length == res.body.data.length);
+    //       });
+    //   });
+    // });
+  });
+
+  describe('DELETE /users/id', function() {
+    let params = {
+          username: 'zenoan',
+          email: 'roger.madjos@gmail.com'
+        };
+
+    let userId;
+    before(function* () {
+      let createResult = yield request
+        .post('/users')
+        .send(params);
+      userId = createResult.body.data._id;
+    });
+
+    describe('Given user was deleted', function() {
+      it('Should return 204', function* () {
+        yield request
+          .delete('/users/'+userId)
+          .expect(204);
+
+          let user = yield UserModel.findById(userId);
+          expect(user).to.be.undefined;
+      });
+    });
+  });
 });
